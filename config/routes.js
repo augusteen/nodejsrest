f71e5de258a2fbbeda9750052416db1467fca0fe
augusteen <austin.chellian@gmail.com>
2016-08-24 16:19:02 +0530

    First commit

---
+++ config/routes.js
@@ -0,0 +1,35 @@
+var employee = require('../model/employee');
+
+
+module.exports = {
+    configure: function(app) {
+        app.get('/', function(req, res, next) {
+            res.render('index', { title: 'Express' });
+        });
+
+        app.get('/', function(req, res, next) {
+            res.send('respond with a resource');
+        });
+
+        app.options('/api/employee/', function(req, res) {
+            employee.get(res);
+        });
+
+ 		app.get('/api/employee/', function(req, res) {
+            employee.get(res);
+        });
+
+        app.post('/api/employee/', function(req, res) {
+            employee.create(req.body, res);
+        });
+
+        app.put('/api/employee/', function(req, res) {
+            employee.update(req.body, res);
+        });
+
+        app.delete('/api/employee/:id/', function(req, res) {
+            employee.delete(req.params.id, res);
+        });
+
+    }
+}
---
+++ model/employee.js
@@ -0,0 +1,55 @@
+var connection = require('../config/config');
+
+var Employee = function() {
+
+    this.get = function(res) {
+        connection.acquire(function(err, con) {
+            con.query('select * from employee', function(err, result) {
+                con.release();
+                res.send(result);
+            });
+        });
+    };
+
+    this.create = function(todo, res) {
+        connection.acquire(function(err, con) {
+            con.query('insert into employee set ?', todo, function(err, result) {
+                con.release();
+                if (err) {
+                    res.send({ status: 1, message: 'TODO creation failed' });
+                } else {
+                    res.send({ status: 0, message: 'TODO created successfully' });
+                }
+            });
+        });
+    };
+
+    this.update = function(todo, res) {
+        connection.acquire(function(err, con) {
+            con.query('update employee set ? where id = ?', [todo, todo.id], function(err, result) {
+                con.release();
+                if (err) {
+                    res.send({ status: 1, message: 'TODO update failed' });
+                } else {
+                    res.send({ status: 0, message: 'TODO updated successfully' });
+                }
+            });
+        });
+    };
+
+    this.delete = function(id, res) {
+        connection.acquire(function(err, con) {
+            con.query('delete from employee where id = ?', [id], function(err, result) {
+                con.release();
+                if (err) {
+                    res.send({ status: 1, message: 'Failed to delete' });
+                } else {
+                    res.send({ status: 0, message: 'Deleted successfully' });
+                }
+            });
+        });
+    };
+
+}
+
+module.exports = new Employee();
